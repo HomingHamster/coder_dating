58f6640842332ffc9cd4125e5df55903924a4970
felix <f@f.com>
2024-06-12 05:29:27 +0100

    models and view plans

---
+++ dating/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+Person
+-firstname
+-bio
+-friendship
+-love
+-work
+-interested_in
+-age
+
+Code
+-person
+-code
+-language
+-date
+
+Opinion
+-by
+-on
+-op_type
+
---
+++ dating/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+Home
+About
+Terms
+BrowsePeople
+EditProfile
+Onboard
+Register
+APIPeople
