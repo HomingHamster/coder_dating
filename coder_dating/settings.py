df1d5f7e1565dac44e9ff0f1fc07b7e873e698f9
felix <f@f.com>
2024-06-17 00:59:21 +0100

    requirements.txt

--- coder_dating/settings.py
+++ coder_dating/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r(l6tgitkau!jo==#_&lux$q&)i)dsht0noac+e9kzrsmkyt4-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
